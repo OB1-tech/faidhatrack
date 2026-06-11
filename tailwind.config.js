52203de1da64f243f8fa5290fcd3c3135a75b208
YRN-V7 <brianomae23@gmail.com>
2026-06-11 11:41:15 +0300

    feat: add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        ink: "#1A1A2E",
+        accent: "#0F7B6C",
+        "accent-l": "#13A88F",
+        gold: "#F4A020",
+      }
+    },
+  },
+  plugins: [],
+}
